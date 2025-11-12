d398d03e51f4ee7c54224c9bd81f8dadb4e6c04a
ayush <ayukshbhattarai@gmail.com>
2025-11-12 11:00:47 +0545

    db tweaks

--- backend/database.py
+++ backend/database.py
@@ -1,24 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# PostgreSQL URI: postgresql://USER:PASSWORD@HOST:PORT/DBNAME
-DATABASE_URI = os.getenv(
-    "DATABASE_URI",
-    "postgresql://ayu:123456@localhost:5432/socialmedia_db",  # default database name
-)
+DATABASE_URI =   "postgresql://ayu:123456@localhost:5432/socialmedia_db", 
 
-# Create SQLAlchemy engine
 engine = create_engine(DATABASE_URI)
 
-# Create a configured "Session" class
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 # Base class for ORM models
 Base = declarative_base()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
